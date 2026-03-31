39aca23ad371cac790e4671d4e6c5d148059bcd2
wbq2007 <3377907620@qq.com>
2026-03-31 18:08:16 +0800

    scenod

--- text.c
+++ text.c
@@ -1,30 +1,10 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-int main() {
-    int target, guess, attempts = 0;
-    srand(time(NULL));
-    target = rand() % 100 + 1; // 1~100的随机数
-
-    printf("欢迎来到猜数字游戏！我已经选好了1~100之间的一个数字。\n");
-
-    do {
-        printf("请输入你的猜测：");
-        scanf("%d", &guess);
-        attempts++;
-
-        if (guess < target) {
-            printf("太小了！\n");
-        }
-        else if (guess > target) {
-            printf("太大了！\n");
-        }
-        else {
-            printf("恭喜你，猜对了！你总共猜了%d次。\n", attempts);
-        }
-    } while (guess != target);
-
-    return 0;
+main()
+{
+	float a, b, c;
+	scanf("%f%f", &a, &b);
+	c = a + b;
+	printf("c=%f", c);
 }
